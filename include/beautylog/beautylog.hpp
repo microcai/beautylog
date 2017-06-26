c63a318674e241be0e4ead40fb1b4a568d96159a
microcai <microcaicai@gmail.com>
2017-06-26 20:35:59 +0800

    first public release

---
+++ include/beautylog/beautylog.hpp
@@ -0,0 +1,126 @@
+﻿
+#pragma once
+
+#include <mutex>
+#include <vector>
+#include <map>
+#include <memory>
+
+#include "boost/date_time/posix_time/posix_time.hpp"
+
+
+#include "fmt_helper.hpp"
+
+namespace beautylog
+{
+	struct log_entry
+	{
+		int priority = 0;
+		boost::posix_time::ptime msg_time;
+		std::string log_message;
+		std::map<std::string, std::string> log_fields;
+	};
+
+	class sd_loger
+	{
+	public:
+		sd_loger();
+		~sd_loger();
+
+		template<typename... Args>
+		void log_with_location(int priority, const char* file, int sourceline, const char* func, const std::string& fmt, Args&&... args)
+		{
+			log_entry this_entry;
+
+			this_entry.log_fields["CODE_FILE"] = file;
+			this_entry.log_fields["CODE_FUNC"] = func;
+			this_entry.log_fields["CODE_LINE"] = std::to_string(sourceline);
+
+			this_entry.msg_time = boost::posix_time::microsec_clock::local_time();
+
+			this_entry.priority = priority;
+
+			this_entry.log_message = format_msg(fmt, std::forward<Args>(args)...);
+
+			if (no_defer)
+			{
+				send_structured_message(this_entry, false);
+			}
+			else
+			{
+				std::unique_lock<std::mutex> l(m_mutex);
+				log_entries.push_back(this_entry);
+			}
+		}
+
+		template<typename... Args>
+		static void log_with_location_but_no_constant_field(int priority, const char* file, int sourceline, const char* func, const std::string& fmt, Args&&... args)
+		{
+			log_entry this_entry;
+
+			this_entry.log_fields["CODE_FILE"] = file;
+			this_entry.log_fields["CODE_FUNC"] = func;
+			this_entry.log_fields["CODE_LINE"] = std::to_string(sourceline);
+
+			this_entry.msg_time = boost::posix_time::microsec_clock::local_time();
+
+			this_entry.priority = priority;
+
+			this_entry.log_message = format_msg(fmt, std::forward<Args>(args)...);
+
+			std::vector<std::string> vector_string;
+
+			vector_string.push_back("PRIORITY=" + std::to_string(this_entry.priority));
+			for (const auto& f : e.log_fields)
+			{
+				vector_string.push_back(f.first + "=" + f.second);
+			}
+
+			vector_string.push_back("MESSAGE=" + e.log_message);
+
+			send_send_structured_message_lines(vector_string);
+		}
+
+		void send_structured_message(const log_entry&e) const;
+
+		static void send_send_structured_message_lines(const std::vector<std::string>& msg_lines);
+
+	public:
+		void flush();
+		void disable_defer();
+		void enable_defer();
+
+		// discard log that has priority<priority
+		void discardlog(int priority = 3);
+
+		void add_constant_field(std::string field_name, std::string field_value);
+		void set_constant_fields(const std::map<std::string, std::string>& cf);
+
+	private:
+		std::map<std::string, std::string> constant_fields;
+		std::vector<log_entry> log_entries;
+		bool no_defer = false;
+		mutable std::mutex m_mutex;
+	};
+} // namespace beautylog
+
+#ifdef _MSC_VER
+#define beautylog_print(loger, priority, fmt, ...) loger->log_with_location(priority, __FILE__, __LINE__, __func__, fmt, __VA_ARGS__)
+#define log_print(priority, fmt, ...) beautylog::log_with_location_but_no_constant_field(priority, __FILE__, __LINE__, __func__, fmt, __VA_ARGS__)
+#else
+#define beautylog_print(loger, priority,  ...) loger->log_with_location(priority, __FILE__, __LINE__, __func__, __VA_ARGS__)
+#define log_print(priority,  ...) beautylog::log_with_location_but_no_constant_field(priority, __FILE__, __LINE__, __func__, __VA_ARGS__)
+#endif
+
+typedef std::shared_ptr<beautylog::sd_loger> sdloger_t;
+
+#ifndef LOG_EMERG
+	#define	LOG_EMERG	0	/* system is unusable */
+	#define	LOG_ALERT	1	/* action must be taken immediately */
+	#define	LOG_CRIT	2	/* critical conditions */
+	#define	LOG_ERR		3	/* error conditions */
+	#define	LOG_WARNING	4	/* warning conditions */
+	#define	LOG_NOTICE	5	/* normal but significant condition */
+	#define	LOG_INFO	6	/* informational */
+	#define	LOG_DEBUG	7	/* debug-level messages */
+#endif
---
+++ src/beautylog.cpp
@@ -0,0 +1,171 @@
+﻿
+#define SD_JOURNAL_SUPPRESS_LOCATION
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <sys/uio.h>
+#include <systemd/sd-journal.h>
+#endif
+
+#include <iostream>
+#include <algorithm>
+#include "beautylog/beautylog.hpp"
+
+namespace beautylog
+{
+	sd_loger::sd_loger()
+	{
+	}
+
+	sd_loger::~sd_loger()
+	{
+		flush();
+	}
+
+#ifdef _WIN32
+	static std::wstring ConvertUTF8ToUTF16(std::string u8str)
+	{
+		std::size_t wchar_length = ::MultiByteToWideChar(
+			CP_UTF8,                // convert from UTF-8
+			0,   // error on invalid chars
+			u8str.c_str(),      // source UTF-8 string
+			(int)u8str.length() + 1, // total length of source UTF-8 string,
+								// in CHAR's (= bytes), including end-of-string \0
+			NULL,               // unused - no conversion done in this step
+			0                   // request size of destination buffer, in WCHAR's
+		);
+
+		if (wchar_length == 0)
+		{
+			return L"";
+		}
+
+		std::wstring wstr;
+
+		wstr.resize(wchar_length);
+
+		//
+		// Do the conversion from UTF-8 to UTF-16
+		//
+		int result = ::MultiByteToWideChar(
+			CP_UTF8,                // convert from UTF-8
+			0,   // error on invalid chars
+			u8str.c_str(),      // source UTF-8 string
+			(int)u8str.length() + 1, // total length of source UTF-8 string,
+
+			&wstr[0],               // destination buffer
+			(int)wchar_length            // size of destination buffer, in WCHAR's
+		);
+
+
+		// Return resulting UTF16 string
+		return wstr;
+	}
+
+
+	void sd_loger::send_send_structured_message_lines(const std::vector<std::string>& msg_lines)
+	{
+		std::string msgline;
+
+		for (auto & s : msg_lines)
+		{
+			msgline += "[" + s + "]";
+		}
+
+		std::wstring l = ConvertUTF8ToUTF16(msgline);
+
+		OutputDebugStringW(l.c_str());
+		std::wcerr << l << std::endl;
+	}
+#else
+	void sd_loger::send_send_structured_message_lines(const std::vector<std::string>& vector_string)
+	{
+		std::vector<iovec> iovecs(vector_string.size());
+
+		for (int i = 0; i < vector_string.size(); i++)
+		{
+			iovecs[i].iov_base = const_cast<char*>(vector_string[i].data());
+			iovecs[i].iov_len = vector_string[i].size();
+		}
+
+		sd_journal_sendv(&iovecs[0], iovecs.size());
+	}
+#endif
+
+	void sd_loger::flush()
+	{
+		std::unique_lock<std::mutex> l(m_mutex);
+		auto log_entries_copyed = std::move(log_entries);
+
+		l.unlock();
+
+		for (auto e : log_entries)
+		{
+			send_structured_message(e);
+		}
+		log_entries.clear();
+	}
+
+	void sd_loger::disable_defer()
+	{
+		no_defer = true;
+		flush();
+	}
+
+	void sd_loger::enable_defer()
+	{
+		no_defer = false;
+	}
+
+
+	void sd_loger::discardlog(int priority)
+	{
+		std::unique_lock<std::mutex> l(m_mutex);
+
+		for (std::vector<log_entry>::iterator it = log_entries.begin(); it != log_entries.end();)
+		{
+			if (it->priority <= priority)
+			{
+				it = log_entries.erase(it);
+			}
+			else
+			{
+				it++;
+			}
+		}
+	}
+
+	void sd_loger::add_constant_field(std::string field_name, std::string field_value)
+	{
+		std::unique_lock<std::mutex> l(m_mutex);
+		constant_fields.insert({ field_name, field_value });
+	}
+
+	void sd_loger::set_constant_fields(const std::map<std::string, std::string>& cf)
+	{
+		std::unique_lock<std::mutex> l(m_mutex);
+		constant_fields = cf;
+	}
+
+	void sd_loger::send_structured_message(const log_entry&e) const
+	{
+		std::unique_lock<std::mutex> l(m_mutex);
+
+		std::vector<std::string> vector_string;
+
+		vector_string.push_back("PRIORITY=" + std::to_string(e.priority));
+		for (const auto& f : e.log_fields)
+		{
+			vector_string.push_back(f.first + "=" + f.second);
+		}
+
+		for (const auto& f : constant_fields)
+		{
+			vector_string.push_back(f.first + "=" + f.second);
+		}
+
+		vector_string.push_back("MESSAGE=" + e.log_message);
+		send_send_structured_message_lines(vector_string);
+	}
+}
